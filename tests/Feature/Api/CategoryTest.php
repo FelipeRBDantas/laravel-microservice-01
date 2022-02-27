50c7be4ba303f08a8d191cc18f028bd9f9a69055
Felipe Dantas <feliperbdantas@outlook.com>
2022-02-27 17:47:38 -0300

    Test: Create Error Get Single Category, Get Single Category, Validations Store Category and Store Category

--- tests/Feature/Api/CategoryTest.php
+++ tests/Feature/Api/CategoryTest.php
@@ -9,6 +9,8 @@ use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    protected $endpoint = '/categories';
+
     /**
      * A basic feature test get categories.
      *
@@ -18,10 +20,68 @@ class CategoryTest extends TestCase
     {
         Category::factory()->count(4)->create();
 
-        $response = $this->getJson('/categories');
+        $response = $this->getJson($this->endpoint);
 
         $response->assertJsonCount(4, 'data');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test error get single category.
+     *
+     * @return void
+     */
+    public function test_error_get_single_category()
+    {
+        $category = 'fake-url';
+
+        $response = $this->getJson("{$this->endpoint}/{$category}");
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * A basic feature test get single category.
+     *
+     * @return void
+     */
+    public function test_get_single_category()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->getJson("{$this->endpoint}/{$category->url}");
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test validations store category.
+     *
+     * @return void
+     */
+    public function test_validations_store_category()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'title' => '',
+            'description' => ''
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * A basic feature test store category.
+     *
+     * @return void
+     */
+    public function test_store_category()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'title' => 'Category 01',
+            'description' => 'Description of category'
+        ]);
+
+        $response->assertStatus(201);
     }
 }
